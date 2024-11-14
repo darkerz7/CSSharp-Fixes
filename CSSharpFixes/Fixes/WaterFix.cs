b3a8aaa2a601f1f5595917e4bda7bd5267375402
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2024-11-14 20:54:16 +0500

    Removed Underwater patch

--- CSSharpFixes/Fixes/WaterFix.cs
+++ CSSharpFixes/Fixes/WaterFix.cs
@@ -29,7 +29,7 @@ public class WaterFix: BaseFix
         {
             "FixWaterFloorJump",
             "WaterLevelGravity",
-            "CategorizeUnderwater"
+            //"CategorizeUnderwater"
         };
     }
 }
