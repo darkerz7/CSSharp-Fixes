0e79d89416016092db8ebb5d8eea1031bebe9da7
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2026-07-10 21:07:22 +0500

    Update 1.2.8
    Update for 2026-07-08 CS2 update

--- CSSharpFixes/Config/ModuleInformation.cs
+++ CSSharpFixes/Config/ModuleInformation.cs
@@ -3,7 +3,7 @@
 public class ModuleInformation
 {
     public static string ModuleName => "CS#Fixes";
-    public static string ModuleVersion => "1.2.7";
+    public static string ModuleVersion => "1.2.8";
     public static string ModuleAuthor => "hypnos <hyps.dev>, DarkerZ [RUS]";
     public static string ModuleDescription => "cs#fixes";
 }
